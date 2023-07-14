df3d7f18a958c3334d883db91dcbd3a0ff9414a5
ouugazz <ougazzou62001@gmail.com>
2023-07-14 08:51:43 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: print numbers of  base16 lowercase
+ *
+ * REturn: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 48; /*48; decemal rep of 0*/
+
+	while (digit <= 102) /*102; decemal rep of f*/
+	{
+		putchar(digit);
+
+		/* after 9 we jump till 96; */
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}

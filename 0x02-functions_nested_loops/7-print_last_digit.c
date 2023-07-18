91f1cdaedf114ed06d09ae462f6179a343007ac5
ouugazz <ougazzou62001@gmail.com>
2023-07-18 16:14:44 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,8 +15,8 @@ int print_last_digit(int n)
 	if (n < 0)
 		lastDigit = -1 * (n % 10);
 	else
-		lasDigit = n % 10;
+		lastDigit = n % 10;
 
 	_putchar(lastDigit + '0');
-	return (lasDigit);
+	return (lastDigit);
 }

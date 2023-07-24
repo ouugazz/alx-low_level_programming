e6aa081a70bd94fcbbc4021a44044096a609f7c6
ouugazz <ougazzou62001@gmail.com>
2023-07-24 05:53:31 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,11 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] *= '\n'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
 
 	i++;
-	for (i /= 2; str[i] *= '\n'; i++)
+	for (i /= 2; str[i] != '\0'; i++)
 	{
 		putchar(str[i]);
 	}
